fff24fc07181ea8126dc044090c759280e7b8c90
uribeIsaac26 <uribearciniegasisaac@gmail.com>
2025-03-05 20:37:54 -0500

    se hace ejercicio sobre ingreso de datos por consola

---
+++ EntradaDatosConsola/ejercicio_receta_cocina.py
@@ -0,0 +1,9 @@
+nombre_receta = input("Ingrese el nombre de la receta: ")
+ingredientes = input("Ingrese los ingredientes de la receta: ")
+tiempo_preparacion = int(input("Por favor ingrese el tiempo de preparacion de la receta en minutos: "))
+difultad = input("Por favor ingrese la dificultad de la receta: ")
+
+print(f'Nombre de la receta: {nombre_receta}')
+print(f'Ingredientes de la receta: {ingredientes}')
+print(f'Tiempo de preparacion: {tiempo_preparacion}')
+print(f'Dificultad de la receta: {difultad}')
